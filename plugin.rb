eed7c762509d31c8eb820e0207a55e572e420a41
SGColdSun <ColdSun@stomped.us>
2015-05-29 01:27:02 -0700

    End?

--- plugin.rb
+++ plugin.rb
@@ -5,4 +5,3 @@
 register_asset "javascripts/discourse/templates/discovery.hbs"
 register_asset "javascripts/discourse/templates/topic.hbs"
 register_custom_html(extraNavItem: "<li id='faq-menu-item'><a href='/faq'>FAQ</a></li>")
-end
