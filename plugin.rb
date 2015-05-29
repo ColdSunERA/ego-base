04e26ec9599748f8abd6d76088e2fbe7f0ee1343
SGColdSun <ColdSun@stomped.us>
2015-05-29 01:29:53 -0700

    Simple Addition First

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,4 @@
 # version: 0.0.1
 # author: ColdSun
 
-register_asset "javascripts/discourse/templates/discovery.hbs"
-register_asset "javascripts/discourse/templates/topic.hbs"
 register_custom_html(extraNavItem: "<li id='faq-menu-item'><a href='/faq'>FAQ</a></li>")
