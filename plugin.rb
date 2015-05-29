7f0dd75b9168ec1cd11ff5fe6f811bed17d06fc3
SGColdSun <ColdSun@stomped.us>
2015-05-29 01:53:35 -0700

    More

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,4 @@
 # version: 0.0.1
 # author: ColdSun
 register_custom_html(extraNavItem: "<li id='faq-menu-item'><a href='/faq'>FAQ Woot</a></li>")
+register_asset "javascripts/discourse/templates/discovery.hbs"
