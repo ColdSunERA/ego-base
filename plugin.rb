d17c38d0c02fba7d1c574f1123118c379b623c36
SGColdSun <ColdSun@stomped.us>
2015-05-29 01:46:54 -0700

    ugh

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Steam authenticationEGO Base Theme For Discourse
 # version: 0.0.1
 # author: ColdSun
-register_asset "/javascripts/discourse/templates/discovery.hbs"
+register_asset "javascripts/discourse/templates/discovery.hbs"
 
 register_custom_html(extraNavItem: "<li id='faq-menu-item'><a href='/faq'>FAQ Woot</a></li>")
